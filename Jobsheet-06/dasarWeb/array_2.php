6448b14fe1b8e8e6ca04ae962aae67a71ad07e73
Khip01 <aakhifathallah24@gmail.com>
2024-10-02 15:14:08 +0700

    add(array): practicum pt.2
    associative array

---
+++ Jobsheet-06/dasarWeb/array_2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+</head>
+
+<body>
+    <?php
+    $Dosen = [
+        'nama' => 'Elok Nur Hamdana',
+        'domisili' => 'Malang',
+        'jenis kelamin' => 'Perempuan'
+    ];
+    echo "Nama : {$Dosen['nama']} <br>";
+    echo "Domisili : {$Dosen['domisili']} <br>";
+    echo "Jenis Kelamin : {$Dosen['jenis kelamin']} <br>";
+    ?>
+</body>
+
+</html>
