49126ce7daed1297c7d61bdb786c9d58728163df
Khip01 <aakhifathallah24@gmail.com>
2024-10-02 15:24:59 +0700

    fix(array): practicum pt.2
    associative array

--- Jobsheet-06/dasarWeb/array_2.php
+++ Jobsheet-06/dasarWeb/array_2.php
@@ -4,7 +4,34 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title></title>
+    <title>Array 2</title>
+    <style>
+        table {
+            width: 50%;
+            border-collapse: collapse;
+            margin-top: 30px;
+        }
+
+        table tr {
+            text-align: center;
+        }
+
+        table tr th {
+            padding: 15px;
+            border: 1px solid black;
+        }
+
+        table tr td {
+            padding: 15px;
+            border: 1px solid black;
+        }
+
+        .centered-div {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+    </style>
 </head>
 
 <body>
@@ -14,10 +41,24 @@
         'domisili' => 'Malang',
         'jenis kelamin' => 'Perempuan'
     ];
-    echo "Nama : {$Dosen['nama']} <br>";
-    echo "Domisili : {$Dosen['domisili']} <br>";
-    echo "Jenis Kelamin : {$Dosen['jenis kelamin']} <br>";
+    // echo "Nama : {$Dosen['nama']} <br>";
+    // echo "Domisili : {$Dosen['domisili']} <br>";
+    // echo "Jenis Kelamin : {$Dosen['jenis kelamin']} <br>";
     ?>
+    <div class="centered-div">
+        <table style="width: 50%;">
+            <tr>
+                <th>Nama</th>
+                <th>Domisili</th>
+                <th>Jenis Kelamin</th>
+            </tr>
+            <tr>
+                <td><?php echo $Dosen['nama']; ?></td>
+                <td><?php echo $Dosen['domisili']; ?></td>
+                <td><?php echo $Dosen['jenis kelamin']; ?></td>
+            </tr>
+        </table>
+    </div>
 </body>
 
 </html>
