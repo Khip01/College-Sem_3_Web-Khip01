be75b855d313ce87e14b9cc058d24cb0c8ca9405
Khip01 <aakhifathallah24@gmail.com>
2024-10-02 16:21:05 +0700

    fix(html): practicum pt.3
    entities html

---
+++ Jobsheet-06/dasarWeb/praktik_php/htmlandphp.php
@@ -0,0 +1,20 @@
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cara 01</title>
+</head>
+<body>
+    <p>Tanggal Hari ini : <?php echo date("d M Y")?></p>
+</body>
+</html> -->
+
+<?php
+echo '<html>';
+echo '<head><title>Cara02</title> </head>';
+echo '<body>';
+echo '<p>Tanggal Hari ini: '.date('d M Y'). '</p>';
+echo '</body>';
+echo '</html>';
+?>
