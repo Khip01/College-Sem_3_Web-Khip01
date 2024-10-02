5c03c9baf25f0c64a24b035da2fabec5f18a0614
Khip01 <aakhifathallah24@gmail.com>
2024-10-02 15:12:21 +0700

    add(array): practicum pt.1
    indexed array

---
+++ Jobsheet-06/dasarWeb/array_1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+</head>
+
+<body>
+    <h2>Array Terindeks</h2>
+    <?php
+    $Listdosen = ["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
+    // echo $Listdosen[2] . "<br>";
+    // echo $Listdosen[0] . "<br>";
+    // echo $Listdosen[1] . "<br>";
+
+    foreach ($Listdosen as $dosen) {
+        echo $dosen . "<br>";
+    }
+    ?>
+</body>
+
+</html>
