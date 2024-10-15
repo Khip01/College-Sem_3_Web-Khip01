dd42eab747ff0dc40320a224650fb8660683474e
Khip01[desk] <aakhifathallah24@gmail.com>
2024-10-15 10:14:53 +0700

    add(upload) practicum pt.1
    upload file

---
+++ jobsheet-08/dasarWeb/upload.php
@@ -0,0 +1,24 @@
+<?php
+if(isset($_POST["submit"])){
+    $targetdir = "uploads/"; //Direktori tujuan untuk menyimpan file
+    $targetfile = $targetdir. basename($_FILES["myfile"]["name"]);
+    $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
+
+    // $allowedExtensions = array("jpg", "jpeg", "png", "gif");
+    // $maxsize = 5*1024*1024;
+    $allowedExtensions = array("txt", "pdf", "doc", "docx");
+    $maxsize = 3*1024*1024;
+    
+    if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
+        if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
+            echo "File berhasil diunggah.";
+            // echo "<br>";
+            // echo "<img src='$targetfile' alt='' width='200'>";
+        } else {
+            echo "Gagal mengunggah file.";
+        }
+    } else {
+        echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan";
+    }
+}
+?>
